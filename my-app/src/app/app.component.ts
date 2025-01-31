e00cada74bf42abe7ff9d5cee75facd03e6e041b
Ashutoshkumar-gh <ashutosh@growhut.in>
2025-01-31 23:00:57 +0530

    pagination added for more than 5 users

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -5,16 +5,17 @@ import { User, Workout } from './app.model';
 import { WorkoutService } from './app.service';
 import { CommonModule } from '@angular/common';
 import { SearchFilterComponent } from './components/search-filter/search-filter.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , FormsModule,CommonModule, SearchFilterComponent],
+  imports: [RouterOutlet, FormsModule, CommonModule, SearchFilterComponent, PaginationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-app';
- 
+
   userName: string = '';
   workoutType: string = '';
   workoutMinutes: number | null = null;
@@ -22,18 +23,21 @@ export class AppComponent {
   total: number = 0;
   filteredUsers: User[] = [];
   paginatedUsers: User[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 5;
+
 
   constructor(private workoutService: WorkoutService) {
     this.users = this.workoutService.getUsers();
     this.filteredUsers = this.users;
-   
+    this.updatePagination();
   }
 
   addWorkout() {
     if (this.userName && this.workoutType && this.workoutMinutes !== null) {
       this.workoutService.addWorkout(this.userName, {
         type: this.workoutType,
-        minutes: this.workoutMinutes
+        minutes: this.workoutMinutes,
       });
       this.users = this.workoutService.getUsers();
       console.log('users:', this.users);
@@ -41,7 +45,6 @@ export class AppComponent {
     }
   }
 
-  
   clearForm() {
     this.userName = '';
     this.workoutType = '';
@@ -49,30 +52,47 @@ export class AppComponent {
   }
 
   calculateTotal(user: { workoutData: Workout[] }): number {
-    return user.workoutData.reduce((total: number, workout: Workout) => total + workout.minutes, 0);
+    return user.workoutData.reduce(
+      (total: number, workout: Workout) => total + workout.minutes,
+      0
+    );
   }
 
   // New method to get workout types as a comma-separated string
   getWorkoutTypes(user: { workoutData: Workout[] }): string {
-    return user.workoutData.map(workout => workout.type).join(', ');
+    return user.workoutData.map((workout) => workout.type).join(', ');
   }
 
   onSearch(searchText: string) {
-    this.filteredUsers = this.users.filter(user =>
+    this.filteredUsers = this.users.filter((user) =>
       user.userName.toLowerCase().includes(searchText.toLowerCase())
     );
     console.log('filteredUsers:', this.filteredUsers);
-    
+    this.currentPage = 1;
+    this.updatePagination();
   }
 
   onFilterChange(workoutType: string) {
     console.log('workoutType:', workoutType);
     this.filteredUsers = workoutType
-      ? this.users.filter(user =>
-          user.workoutData.some(workout => workout.type === workoutType)
+      ? this.users.filter((user) =>
+          user.workoutData.some((workout) => workout.type === workoutType)
         )
       : this.users;
 
-      console.log('filteredUsers:', this.filteredUsers);
-      
-}}
+    console.log('filteredUsers:', this.filteredUsers);
+    this.currentPage = 1;
+    this.updatePagination();
+  }
+
+  onPageChange(page: number) {
+    this.currentPage = page;
+    this.updatePagination();
+  }
+
+  updatePagination() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedUsers = this.filteredUsers.slice(startIndex, endIndex);
+  }
+}
---
+++ my-app/src/app/components/pagination/pagination.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  imports: [],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.css'
+})
+export class PaginationComponent {
+  @Input() totalItems: number = 0;
+  @Input() itemsPerPage: number = 5;
+  @Input() currentPage: number = 1;
+  @Output() pageChanged = new EventEmitter<number>();
+
+  get totalPages(): number {
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+
+  changePage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.pageChanged.emit(page);
+    }
+  }
+
+}
